700fd0acee3659abdfad5467854790457812ce30
Claudia Pando <claudiamfp@hotmail.com>
2024-10-04 08:37:10 +0100

    solved labs

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,13 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+    if(num1 > num2){
+        return num1 }
+        else if ( num1 < num2){
+            return num2
+        }
+        else if ( num1 === num2){
+            return num1}
+}
 
 
 
@@ -7,23 +15,50 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
-
+function findLongestWord(words) {
+    if (words.length === 0) {
+      return null;
+    }
+    let longestWord = words[0];
+    words.forEach(function(word) {
+      if (word.length > longestWord.length) {
+        longestWord = word;
+      }
+    });
+    return longestWord;
+  }
+  console.log(findLongestWord(words));
 
 
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+  let sum= 0;
 
+  for(i=0; i< numbers.length; i++){
+    sum+= numbers[i];
+  }
+return sum;
+}
 
 
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers2) {
+  let sum=0;
+ 
+  for(i=0; i< numbers2.length; i++){
+    sum += numbers2[i];
+  }
+  if (numbers2.length === 0){
+    return 0;
+  }
+return sum /numbers2.length ;
+}
 
 
 
@@ -31,4 +66,18 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(words2, wordToSearch){
+  if (words2.length === 0){
+    return null;
+  }
+
+  for(let i=0; i< words2.length;i++){
+    if (words2 [i] === wordToSearch){
+      return true;
+}
+
+else if (i === words2.length-1){
+  return false;
+}
+  }
+}
